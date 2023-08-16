0f10aae4f75e82c1c680b46581b81707d1a30f77
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-08-17 03:27:53 +0530

    done

--- tdd/urls.py
+++ tdd/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('temprature/<str:city>/', views.get_cite, name='get_city'),
-   
+    path("temprature/<str:city>/", views.get_cite, name="get_city"),
+    path("addcity/", views.create_city, name="create_city"),
+    path("delete/<str:city>/", views.delete_city, name="delete_city"),
+    path("update/<str:city>/", views.update_city, name="update_city"),
 ]
--- tdd/views.py
+++ tdd/views.py
@@ -1,12 +1,66 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 import json
-# Create your views here.
-def get_cite(request, city):
+
+# Read city information
+def get_city(request, city):
     with open("tdd\db.json") as json_file:
         data = json.load(json_file)
         for city_data in data["cities"]:
             if city_data["name"] == city:
                 return JsonResponse(city_data)
-        return JsonResponse({"msg": "data not found!"})
+        return JsonResponse({"msg": "City not found!"}, status=404)
 
+# Create a new city
+@csrf_exempt
+@require_POST
+def create_city(request):
+    data = json.loads(request.body)
+    with open("tdd\db.json") as json_file:
+        cities_data = json.load(json_file)
+    
+    new_city = {
+        "name": data.get("name"),
+        "temperature": data.get("temperature")
+    }
+    
+    cities_data["cities"].append(new_city)
+    
+    with open("tdd\db.json", "w") as json_file:
+        json.dump(cities_data, json_file, indent=4)
+    
+    return JsonResponse(new_city, status=201)
+
+# Update city information
+@csrf_exempt
+@require_POST
+def update_city(request, city):
+    data = json.loads(request.body)
+    with open("tdd\db.json") as json_file:
+        cities_data = json.load(json_file)
+    
+    for city_data in cities_data["cities"]:
+        if city_data["name"] == city:
+            city_data["temperature"] = data.get("temperature")
+            with open("tdd\db.json", "w") as json_file:
+                json.dump(cities_data, json_file, indent=4)
+            return JsonResponse(city_data)
+    
+    return JsonResponse({"msg": "City not found!"}, status=404)
+
+# Delete city
+@csrf_exempt
+def delete_city(request, city):
+    with open("tdd\db.json") as json_file:
+        cities_data = json.load(json_file)
+    
+    for i, city_data in enumerate(cities_data["cities"]):
+        if city_data["name"] == city:
+            deleted_city = cities_data["cities"].pop(i)
+            with open("tdd\db.json", "w") as json_file:
+                json.dump(cities_data, json_file, indent=4)
+            return JsonResponse(deleted_city)
+    
+    return JsonResponse({"msg": "City not found!"}, status=404)
